68adc6aa6ffb211015858f52675e3e29ba24184d
CubeMaterial <maestro.master03@gmail.com>
2025-12-30 12:55:08 +0900

    firebase setting
    파이어베이스 디폴트 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:project_pairs_251230/firebase_options.dart';
 import 'package:project_pairs_251230/view/product/main_page.dart';
 
 void main() async  {
