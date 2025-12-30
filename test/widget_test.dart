5b804e4d7d4864146cd0a4d022d10b7f20b2711c
boksir1 <gqymh2g2st@privaterelay.appleid.com>
2025-12-30 12:13:09 +0900

    test_push_7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:project_pairs_251230/main.dart';
+import 'package:project_pairs_251230/view/product/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
