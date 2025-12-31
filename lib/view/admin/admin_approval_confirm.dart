6b375c4200ece237224e42e85269d6147cb16ff9
혜전 <gracefield0814@gmail.com>
2025-12-31 17:05:13 +0900

    이미지 파일 생성, 샘플 이미지 등록, 링크 페이지 생성.

---
+++ lib/view/admin/admin_approval_confirm.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminApprovalConfirm extends StatefulWidget {
+  const AdminApprovalConfirm({super.key});
+
+  @override
+  State<AdminApprovalConfirm> createState() => _AdminApprovalConfirmState();
+}
+
+class _AdminApprovalConfirmState extends State<AdminApprovalConfirm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
