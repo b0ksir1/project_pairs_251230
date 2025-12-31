ab093d6c22e7de89c9236ca21237f0a00d49ee59
Oh seongui <34715603@naver.com>
2025-12-31 12:25:29 +0900

    storeDB 생성, manufactureDB 수정

---
+++ Python/storeDB.py
@@ -0,0 +1,84 @@
+from fastapi import APIRouter, Form
+import pymysql
+router = APIRouter()
+
+fastAPIAddress = "172.16.250.178"
+dbAddress = "172.16.250.171"
+
+def connect():
+    return pymysql.connect(
+        host=dbAddress,
+        user="root",
+        password="qwer1234",
+        database="project_onandtap",
+        charset="utf8"
+    )
+
+@router.get('/select')
+async def select():
+    conn = connect()
+    curs = conn.cursor()    
+    curs.execute("""
+            select store_id, store_name, store_phone, store_lat, store_lng
+            from store
+            order by store_name
+            """
+    )
+    rows = curs.fetchall()
+    conn.close()
+
+    result = [{'store_id' : row[0], 'store_name' : row[1], 'store_phone' : row[3], 'store_lat' : row[4], 'store_lng' : row[5]} for row in rows]
+    return {'results' : result}
+
+@router.post('/insert')
+async def insert(store_name : str = Form(...), store_phone : str = Form(...), store_lat : float = Form(...), store_lng : float = Form(...)):
+    try:
+        conn = connect()
+        curs = conn.cursor()
+        sql = """
+            insert into store
+            (store_name, store_phone, store_lat, store_lng)
+            values
+            (%s, %s, %s, %s)
+            """
+        curs.execute(sql, (store_name, store_phone, store_lat, store_lng))
+        conn.commit()
+        conn.close()
+        return {'results' : "OK"}
+
+    except Exception as e:
+        print("Error", e)
+        return {'results' : "Error"}  
+    
+@router.post('/update')
+async def update(store_id : int = Form(...), store_name : str = Form(...), store_phone : str = Form(...), store_lat : float = Form(...), store_lng : float = Form(...)):
+    try:
+        conn = connect()
+        curs = conn.cursor()
+        sql = """
+            update store
+            set store_name = %s, store_phone = %s, store_lat =%s, store_lng =%s
+            where store_id = %s
+            """
+        curs.execute(sql, (store_name, store_phone, store_lat, store_lng, store_id))
+        conn.commit()
+        conn.close()
+        return {'results' : "OK"}
+
+    except Exception as e:
+        print("Error", e)
+        return {'results' : "Error"} 
+    
+@router.delete('/delete/{store_id}')
+async def delete(store_id : int):
+    try:
+        conn = connect()
+        curs = conn.cursor()
+        curs.execute("delete from store where store_id = %s", (store_id))
+        conn.commit()
+        conn.close()
+        return {'results' : "OK"}
+
+    except Exception as e:
+        print("Error ", e)
+        return {'results' : "Error"}  
