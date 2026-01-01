0152a0eb522cca2c8392356e28923e172b91e880
고석민 <80875799+ksm0210@users.noreply.github.com>
2026-01-01 12:49:05 +0900

    customer order by뺌, DB서버변경

--- Python/config.py
+++ Python/config.py
@@ -1,7 +1,7 @@
 # config.py
-DB_HOST = "172.16.250.171"
+DB_HOST = "210.99.163.45"
 DB_USER = "root"
 DB_PASSWORD = "qwer1234"
 DB_NAME = "mydb"
 
-FASTAPI_HOST = "172.16.250.179"
+FASTAPI_HOST = "210.99.163.45"
