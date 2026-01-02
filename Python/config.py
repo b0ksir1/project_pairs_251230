c2d026d516781f0a6c1d4f5786453c4596dc91c8
SuJin Park <overblac@naver.com>
2026-01-02 10:14:32 +0900

    Update config

--- Python/config.py
+++ Python/config.py
@@ -1,7 +1,7 @@
 # config.py
-DB_HOST = "172.16.250.171"
+DB_HOST = "172.30.1.78"
 DB_USER = "root"
 DB_PASSWORD = "qwer1234"
 DB_NAME = "mydb"
 
-FASTAPI_HOST = "172.16.250.194"
+FASTAPI_HOST = "172.30.1.78"
