ed91308ba920fa1bfc405d7455c099637c41f407
boksir1 <gqymh2g2st@privaterelay.appleid.com>
2026-01-02 16:06:02 +0900

    searchpush

--- lib/view/product/main_page.dart
+++ lib/view/product/main_page.dart
@@ -5,6 +5,7 @@ import 'package:project_pairs_251230/util/message.dart';
 import 'package:project_pairs_251230/view/order/shopping_cart.dart';
 import 'package:project_pairs_251230/view/product/main_page_home.dart';
 import 'package:project_pairs_251230/view/search/search_result.dart';
+import 'package:project_pairs_251230/view/user/my_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
