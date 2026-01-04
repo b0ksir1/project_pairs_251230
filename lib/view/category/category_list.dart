8ed1a06b7156515948db8cee7d93027cd93955b6
boksir1 <gqymh2g2st@privaterelay.appleid.com>
2026-01-05 03:34:38 +0900

    발주

--- lib/view/category/category_list.dart
+++ lib/view/category/category_list.dart
@@ -1,11 +1,11 @@
-import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:project_pairs_251230/model/category.dart';
 import 'package:project_pairs_251230/model/product_by_category.dart';
 import 'package:project_pairs_251230/util/global_data.dart';
 import 'package:http/http.dart' as http;
+import 'package:project_pairs_251230/view/product/product_detail.dart';
 
 class CategoryList extends StatefulWidget {
   const CategoryList({super.key});
@@ -15,53 +15,10 @@ class CategoryList extends StatefulWidget {
 }
 
 class _CategoryListState extends State<CategoryList> {
-  // Property
   final TextEditingController searchController = TextEditingController();
   List<Category> categoryList = [];
   int categoryListIndex = 0;
   List<ProductByCategory> productList = [];
-
-  // 카테고리 조회
-  Future<void> getcategoryData() async{
-    var url = Uri.parse("${GlobalData.url}/category/select");
-    var response = await http.get(url);
-    categoryList.clear();
-    var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
-    List result = dataConvertedJSON["results"];
-    for(var item in result){
-      Category category = Category(
-        category_name: item['category_name']
-      );
-      categoryList.add(category);
-    }
-
-    setState(() {});
-  }
-
-  // 제품 브랜드 별 조회
-  Future<void> getProductData() async{
-    var url = Uri.parse("${GlobalData.url}/product/products");
-    var response = await http.get(url);
-    productList.clear();
-    var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
-    List result = dataConvertedJSON["results"];
-    for(var item in result){
-      print(item);
-      var brand = item['products'];
-      print(brand);
-      for(var p in brand)
-      {
-        ProductByCategory productByCategory = ProductByCategory(
-          brand_name: p['brand_name'],
-          product_id: p['product_id'],
-          product_name: p['product_name'],
-          product_price: p['product_price']
-        );
-      productList.add(productByCategory);
-      }
-    }
-    setState(() {});
-  }
 
   @override
   void initState() {
@@ -70,123 +27,169 @@ class _CategoryListState extends State<CategoryList> {
     getProductData();
   }
 
+  Future<void> getcategoryData() async {
+    var url = Uri.parse("${GlobalData.url}/category/select");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = json.decode(utf8.decode(response.bodyBytes));
+      List result = data["results"];
+      setState(() {
+        categoryList = result.map((e) => Category(category_name: e['category_name'])).toList();
+      });
+    }
+  }
+
+  Future<void> getProductData() async {
+    var url = Uri.parse("${GlobalData.url}/product/products");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = json.decode(utf8.decode(response.bodyBytes));
+      List results = data["results"] ?? [];
+      productList.clear();
+
+      for (var brand in results) {
+        String bName = brand['brand_name'] ?? 'Brand';
+        int? bId = brand['brand_id'];
+        List pItems = brand['products'] ?? [];
+
+        for (var p in pItems) {
+          productList.add(ProductByCategory(
+            brand_id: bId,
+            brand_name: bName,
+            product_id: p['product_id'],
+            product_name: p['product_name'] ?? '',
+            product_price: p['product_price'] ?? 0,
+          ));
+        }
+      }
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(
-          '카테고리',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 20),
+          onPressed: () => Get.back(),
         ),
+        title: const Text('카테고리 탐색', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
         centerTitle: true,
-        
       ),
       body: Column(
         children: [
           _buildSearchBar(),
           _buildTabs(),
-          SizedBox(
-            height: 300,
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-                childAspectRatio: 1,
-              ),
-              itemCount: productList.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Text(productList[index].brand_name),
-                      Text(productList[index].product_name),
-                      Text('${productList[index].product_price}'),
-                    ],
+          const SizedBox(height: 10),
+          Expanded(
+            child: productList.isEmpty
+                ? const Center(child: CircularProgressIndicator(color: Colors.black))
+                : GridView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 24,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemCount: productList.length,
+                    itemBuilder: (context, index) => _buildProductItem(index),
                   ),
-                );
-              },
-            ),
           )
         ],
       ),
     );
-  } //build
-
-  // --- UI: 검색 바 ---
-  Widget _buildSearchBar() {
-    return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.all(20),
-      child: TextField(
-        controller: searchController,
-        onChanged: (_) => setState(() {}),
-        decoration: InputDecoration(
-          hintText: '카테고리 검색',
-          prefixIcon: Icon(
-            Icons.search,
-            color: Colors.grey,
-          ),
-          filled: true,
-          fillColor: Colors.grey[200],
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 12,
-            vertical: 12,
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide.none,
-          ),
-        ),
-      ),
-    );
   }
 
-  // --- UI: 카테고리 탭 ---
-  Widget _buildTabs() {
-    return Container(
-      color: Colors.white,
-      padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: List.generate(categoryList.length, (i) {
-            final selected = i == categoryListIndex;
-            
-            return Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: GestureDetector(
-                onTap: () => setState(() => categoryListIndex = i),
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 200),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 14,
-                    vertical: 10,
-                  ),
-                  decoration: BoxDecoration(
-                    color: selected ? Colors.black :  Colors.white,
-                    borderRadius: BorderRadius.circular(100),
-                    border: selected ? Border.all(color: Colors.black) : Border.all(color: Colors.grey.shade300)
-                  ),
-                  child: Text(
-                    categoryList[i].category_name,
-                    style: TextStyle(
-                      color: selected ? Colors.white : Colors.grey[700],
-                      fontWeight: FontWeight.w700,
-                      fontSize: 14,
-                    ),
-                  ),
+  Widget _buildProductItem(int index) {
+    final p = productList[index];
+    return GestureDetector(
+      onTap: () => Get.to(() => const ProductDetail(), arguments: p.product_id),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFF5F5F5),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.network(
+                  '${GlobalData.url}/images/view/${p.product_id}',
+                  fit: BoxFit.cover,
+                  errorBuilder: (c, e, s) => const Icon(Icons.image_not_supported, color: Colors.grey),
                 ),
               ),
-            );
-          }),
+            ),
+          ),
+          const SizedBox(height: 12),
+          Text(p.brand_name, style: const TextStyle(fontSize: 12, color: Colors.grey, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 4),
+          Text(p.product_name, maxLines: 1, overflow: TextOverflow.ellipsis, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 6),
+          Text(
+            '₩${p.product_price.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}',
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w900),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        height: 52,
+        decoration: BoxDecoration(color: const Color(0xFFF8F8F8), borderRadius: BorderRadius.circular(30)),
+        child: TextField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            hintText: '원하시는 카테고리나 상품 검색',
+            hintStyle: TextStyle(color: Colors.grey, fontSize: 13),
+            prefixIcon: Icon(Icons.search, color: Colors.black, size: 20),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 15),
+          ),
         ),
       ),
     );
   }
 
-} // class
+  Widget _buildTabs() {
+    return SizedBox(
+      height: 48,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        scrollDirection: Axis.horizontal,
+        itemCount: categoryList.length,
+        separatorBuilder: (c, i) => const SizedBox(width: 10),
+        itemBuilder: (context, index) {
+          final sel = index == categoryListIndex;
+          return GestureDetector(
+            onTap: () => setState(() => categoryListIndex = index),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                color: sel ? Colors.black : Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: sel ? Colors.black : const Color(0xFFEEEEEE)),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                categoryList[index].category_name,
+                style: TextStyle(color: sel ? Colors.white : Colors.black, fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/vm/splash_screen.dart
+++ lib/vm/splash_screen.dart
@@ -1,87 +1,194 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:project_pairs_251230/view/auth/customer_login.dart';
+import 'package:project_pairs_251230/model/category.dart';
+import 'package:project_pairs_251230/model/product_by_category.dart';
+import 'package:project_pairs_251230/util/global_data.dart';
+import 'package:http/http.dart' as http;
+import 'package:project_pairs_251230/view/product/product_detail.dart';
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+class CategoryList extends StatefulWidget {
+  const CategoryList({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<CategoryList> createState() => _CategoryListState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  double _opacity = 0;
+class _CategoryListState extends State<CategoryList> {
+  final TextEditingController searchController = TextEditingController();
+  List<Category> categoryList = [];
+  int categoryListIndex = 0;
+  List<ProductByCategory> productList = [];
 
   @override
   void initState() {
     super.initState();
-    _startSplash();
+    getcategoryData();
+    getProductData();
   }
 
-  void _startSplash() async {
-    await Future.delayed(const Duration(milliseconds: 500));
-    if (mounted) setState(() => _opacity = 1);
-    
-    await Future.delayed(const Duration(seconds: 2));
-    Get.off(() => const CustomerLogin()); 
+  Future<void> getcategoryData() async {
+    var url = Uri.parse("${GlobalData.url}/category/select");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = json.decode(utf8.decode(response.bodyBytes));
+      List result = data["results"];
+      setState(() {
+        categoryList = result.map((e) => Category(category_name: e['category_name'])).toList();
+      });
+    }
+  }
+
+  Future<void> getProductData() async {
+    var url = Uri.parse("${GlobalData.url}/product/products");
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var data = json.decode(utf8.decode(response.bodyBytes));
+      List results = data["results"] ?? [];
+      productList.clear();
+
+      for (var brand in results) {
+        String bName = brand['brand_name'] ?? 'Brand';
+        int? bId = brand['brand_id'];
+        List pItems = brand['products'] ?? [];
+
+        for (var p in pItems) {
+          productList.add(ProductByCategory(
+            brand_id: bId,
+            brand_name: bName,
+            product_id: p['product_id'],
+            product_name: p['product_name'] ?? '',
+            product_price: p['product_price'] ?? 0,
+          ));
+        }
+      }
+      setState(() {});
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: AnimatedOpacity(
-          opacity: _opacity,
-          duration: const Duration(seconds: 1),
-          curve: Curves.easeIn,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 180,
-                height: 180,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 20,
-                      offset: Offset(0, 10),
-                    )
-                  ],
-                ),
-                child: const Center(
-                  child: Icon(
-                    Icons.shopping_bag_outlined,
-                    color: Colors.white,
-                    size: 80,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 20),
+          onPressed: () => Get.back(),
+        ),
+        title: const Text('카테고리 탐색', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          _buildSearchBar(),
+          _buildTabs(),
+          const SizedBox(height: 10),
+          Expanded(
+            child: productList.isEmpty
+                ? const Center(child: CircularProgressIndicator(color: Colors.black))
+                : GridView.builder(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 24,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemCount: productList.length,
+                    itemBuilder: (context, index) => _buildProductItem(index),
                   ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildProductItem(int index) {
+    final p = productList[index];
+    return GestureDetector(
+      onTap: () => Get.to(() => const ProductDetail(), arguments: p.product_id),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFF5F5F5),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.network(
+                  '${GlobalData.url}/images/view/${p.product_id}',
+                  fit: BoxFit.cover,
+                  errorBuilder: (c, e, s) => const Icon(Icons.image_not_supported, color: Colors.grey),
                 ),
               ),
-              const SizedBox(height: 40),
-              const Text(
-                'On & Tap',
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  letterSpacing: -1.5,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Online Payment, Tap for Pickup',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black54,
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
-            ],
+            ),
+          ),
+          const SizedBox(height: 12),
+          Text(p.brand_name, style: const TextStyle(fontSize: 12, color: Colors.grey, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 4),
+          Text(p.product_name, maxLines: 1, overflow: TextOverflow.ellipsis, style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 6),
+          Text(
+            '₩${p.product_price.toString().replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => '${m[1]},')}',
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w900),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearchBar() {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Container(
+        height: 52,
+        decoration: BoxDecoration(color: const Color(0xFFF8F8F8), borderRadius: BorderRadius.circular(30)),
+        child: TextField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            hintText: '원하시는 카테고리나 상품 검색',
+            hintStyle: TextStyle(color: Colors.grey, fontSize: 13),
+            prefixIcon: Icon(Icons.search, color: Colors.black, size: 20),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.symmetric(vertical: 15),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTabs() {
+    return SizedBox(
+      height: 48,
+      child: ListView.separated(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        scrollDirection: Axis.horizontal,
+        itemCount: categoryList.length,
+        separatorBuilder: (c, i) => const SizedBox(width: 10),
+        itemBuilder: (context, index) {
+          final sel = index == categoryListIndex;
+          return GestureDetector(
+            onTap: () => setState(() => categoryListIndex = index),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                color: sel ? Colors.black : Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: sel ? Colors.black : const Color(0xFFEEEEEE)),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                categoryList[index].category_name,
+                style: TextStyle(color: sel ? Colors.white : Colors.black, fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
