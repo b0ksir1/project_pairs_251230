0866d5b7248478767f9acb12f7fa2fddcdc62903
boksir1 <gqymh2g2st@privaterelay.appleid.com>
2026-01-05 12:19:57 +0900

    메인페이지 viewone적용

--- lib/util/global_data.dart
+++ lib/util/global_data.dart
@@ -1,4 +1,4 @@
 class GlobalData {
-  static String url = 'http://192.168.219.103:8000'; // "192.168.0.24"
+  static String url = 'http://172.16.250.96:8000'; // "192.168.0.24"
   static int? customerId;
 }
